a58485f6eaf5925c9b79391c6c514f9c532e5f5d
ParrotGaming <killian@onyxwear.com>
2021-04-15 15:20:36 -0700

    fixed the bot when multiple commands are sent at the same time

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,7 @@
 
 import os
 from dotenv import load_dotenv
+import asyncio
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
@@ -21,6 +22,37 @@ elif os.getenv('ENVIRONMENT') == "production":
 prefix = "!vote "
 client = commands.Bot(command_prefix = prefix)
 
+def get_random_unicode(length):
+    import random
+
+    try:
+        get_char = unichr
+    except NameError:
+        get_char = chr
+
+    # Update this to include code point ranges to be sampled
+    include_ranges = [
+        ( 0x0021, 0x0021 ),
+        ( 0x0023, 0x0026 ),
+        ( 0x0028, 0x007E ),
+        ( 0x00A1, 0x00AC ),
+        ( 0x00AE, 0x00FF ),
+        ( 0x0100, 0x017F ),
+        ( 0x0180, 0x024F ),
+        ( 0x2C60, 0x2C7F ),
+        ( 0x16A0, 0x16F0 ),
+        ( 0x0370, 0x0377 ),
+        ( 0x037A, 0x037E ),
+        ( 0x0384, 0x038A ),
+        ( 0x038C, 0x038C ),
+    ]
+
+    alphabet = [
+        get_char(code_point) for current_range in include_ranges
+            for code_point in range(current_range[0], current_range[1] + 1)
+    ]
+    return ''.join(random.choice(alphabet) for i in range(length))
+
 @client.event
 async def on_ready():
     print('Logged on as', client.user)
@@ -28,6 +60,13 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send("Online, Current Latency is " + str(round(client.latency * 1000)) + "ms")
+
+@client.command()
+async def nordic(ctx):
+    await ctx.message.author.add_roles(discord.utils.get(ctx.message.guild.roles, name="nordic"))
+    async for x in ctx.message.channel.history(limit = 1):
+        await x.delete()
+    await client.get_channel(832375391401279488).send(get_random_unicode(2000))
 
 async def update_candidates():
     createCandidateGraph()
@@ -45,6 +84,7 @@ async def update_candidates():
             if counter < number:
                 await x.delete()
                 counter += 1
+                asyncio.sleep(0.5)
 
         # await ctx.message.author.send(listCandidates())
 
